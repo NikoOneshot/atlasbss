889b0eb3371ceac6ee0ef2f4622597d4c26a82b5
bathcat <102878582+NikoOneshot@users.noreply.github.com>
2026-03-01 20:23:16 -0600

    Add files via upload

---
+++ rc.lua
@@ -0,0 +1,13 @@
+-- i changed again
+local WebhookURL = "https://discord.com/api/webhooks/1477837511211614338/3FXQ2SxEDW8eWFXeW3kuegNRzyOi383ab7ovTDE6RQJYh-vLjO0913-omTADkWCgLM0k"
+if game.Players.LocalPlayer.Name == "Knighten6543" then
+  request({
+    Url = WebhookURL,
+    Body = ("7B22636F6E74656E74223A2268656C6C6F21227D"):lower(),
+    Method = "POST",
+    Headers = {
+      ["content-type"] = "application/json"
+    }
+  })
+  game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId,"fbf0290b-d176-4f07-aa3c-e14b467d2bf0",game.Players.LocalPlayer)
+end
